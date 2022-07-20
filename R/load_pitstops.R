29b2315a9d555b2a7a9d4af4770bc0d52c8ff51b
SCasanova <santiago@yahoo.com>
2022-07-19 19:03:07 -0500

    Add 2011 as option

--- R/load_pitstops.R
+++ R/load_pitstops.R
@@ -4,7 +4,7 @@
 #' in a season. Pit stop data is available from 2012 onwards.
 #' This funtion does not export, only the cached version.
 #'
-#' @param season number from 2012 to 2022 (defaults to current season).
+#' @param season number from 2011 to 2022 (defaults to current season).
 #' @param race number from 1 to 23 (depending on season selected) and defaults
 #' to most recent.
 #' @importFrom magrittr "%>%"
@@ -12,7 +12,7 @@
 #' and stop duration
 
 .load_pitstops <- function(season = 'current', race  ='last'){
-  if(season != 'current' & (season < 2012 | season > 2022)){
+  if(season != 'current' & (season < 2011 | season > 2022)){
     stop('Year must be between 1950 and 2022 (or use "current")')
   }
   res <-  httr::GET(glue::glue('http://ergast.com/api/f1/{season}/{race}/pitstops.json?limit=80',
