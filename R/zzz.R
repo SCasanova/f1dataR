46c14ac03d57ca989d520d43f4c9651001254e05
SCasanova <santiago@yahoo.com>
2022-07-27 13:36:12 -0500

    Only install no import

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 
 .onLoad <- function(libname,pkgname){
   reticulate::py_install("fastf1", method = 'auto', pip = T)
-  reticulate::import("fastf1", delay_load = TRUE)
+  # reticulate::import("fastf1", delay_load = TRUE)
 }
