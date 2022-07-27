87a56a0824b354da32dc310c8b34b16473616dbe
SCasanova <santiago@yahoo.com>
2022-07-27 13:37:32 -0500

    conditional install

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 
 .onLoad <- function(libname,pkgname){
-  reticulate::py_install("fastf1", method = 'auto', pip = T)
+  if(!reticulate::py_module_available('fastf1'))
+    reticulate::py_install("fastf1", method = 'auto', pip = T)
   # reticulate::import("fastf1", delay_load = TRUE)
 }
