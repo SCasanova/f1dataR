5d351daccb1e145e3f06e1abcde5382efba93428
Santiago Casanova <santiago.casanova@yahoo.com>
2022-10-04 16:31:59 -0500

    config python

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 
 .onLoad <- function(libname,pkgname){
+  reticulate::configure_environment(pkgname)
   if(!reticulate::py_module_available('fastf1'))
     reticulate::py_install("fastf1", method = 'auto', pip = T)
   # reticulate::import("fastf1", delay_load = TRUE)
