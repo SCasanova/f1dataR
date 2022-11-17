ae6aea0770d447ccb620a52b0464040493071eee
Philip Bulsink <bulsinkp@gmail.com>
2022-11-17 11:30:31 -0500

    Two cache-update fixes (manual testing didn't catch these issues)

--- R/clear_f1_cache.R
+++ R/clear_f1_cache.R
@@ -6,7 +6,7 @@
 #' @export
 
 clear_f1_cache <- function(){
-  reticulate::py_run_string(glue::glue('fastf1.api.Cache.clear_cache("{cache_dir}")', cache_dir = getOption('f1dataR.cache'))
+  reticulate::py_run_string(glue::glue('fastf1.api.Cache.clear_cache("{cache_dir}")', cache_dir = getOption('f1dataR.cache')))
   memoise::forget(f1dataR::load_drivers)
   memoise::forget(f1dataR::load_laps)
   memoise::forget(f1dataR::load_pitstops)
