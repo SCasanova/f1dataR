e76d8e1b9372afff0ac7fe2b4dacd122ef41315f
Philip Bulsink <bulsinkp@gmail.com>
2022-11-28 12:38:08 -0500

    Started adding `.data$` to dplyr calls
    Brings r cmd check into its happy place.

--- R/load_schedule.R
+++ R/load_schedule.R
@@ -5,6 +5,7 @@
 #'
 #' @param season number from 1950 to current season (defaults to current season).
 #' @importFrom magrittr "%>%"
+#' @importFrom rlang .data
 #' @return A dataframe with columns season, round, circuitId, circuitName,
 #' latitute and Longitude, locality (city usually), country, date, and time
 #' of the race.
@@ -17,36 +18,36 @@
       httr::GET(glue::glue('http://ergast.com/api/f1/{season}.json?limit=30', season = season))
     data <- jsonlite::fromJSON(rawToChar(res$content))
     data$MRData$RaceTable$Races %>%
-      tidyr::unnest(cols = c(Circuit), names_repair = 'universal') %>%
+      tidyr::unnest(cols = c(.data$Circuit), names_repair = 'universal') %>%
       janitor::clean_names() %>%
       suppressWarnings() %>%
       suppressMessages() %>%
-      tidyr::unnest(cols = c(location)) %>%
-      dplyr::select(season,
-                    round,
-                    race_name,
-                    circuit_id,
-                    circuit_name,
-                    lat:country,
-                    date) %>%
+      tidyr::unnest(cols = c(.data$location)) %>%
+      dplyr::select(.data$season,
+                    .data$round,
+                    .data$race_name,
+                    .data$circuit_id,
+                    .data$circuit_name,
+                    .data$lat:.data$country,
+                    .data$date) %>%
       tibble::as_tibble()
   } else{
     res <-  httr::GET(glue::glue('http://ergast.com/api/f1/{season}.json?limit=30', season = season))
     data <- jsonlite::fromJSON(rawToChar(res$content))
     data$MRData$RaceTable$Races %>%
-      tidyr::unnest(cols = c(Circuit), names_repair = 'universal') %>%
+      tidyr::unnest(cols = c(.data$Circuit), names_repair = 'universal') %>%
       janitor::clean_names() %>%
       suppressWarnings() %>%
       suppressMessages() %>%
-      tidyr::unnest(cols = c(location)) %>%
-      dplyr::select(season,
-                    round,
-                    race_name,
-                    circuit_id,
-                    circuit_name,
-                    lat:country,
-                    date,
-                    time) %>%
+      tidyr::unnest(cols = c(.data$location)) %>%
+      dplyr::select(.data$season,
+                    .data$round,
+                    .data$race_name,
+                    .data$circuit_id,
+                    .data$circuit_name,
+                    .data$lat:.data$country,
+                    .data$date,
+                    .data$time) %>%
       tibble::as_tibble()
    }
 
