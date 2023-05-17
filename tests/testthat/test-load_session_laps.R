a32288b46bed5ba6653d2ee92444627ef5a31560
Philip Bulsink <bulsinkp@gmail.com>
2023-05-16 20:33:59 -0400

    Added quali splitting & changed function name

--- tests/testthat/test-load_session_laps.R
+++ tests/testthat/test-load_session_laps.R
@@ -5,7 +5,7 @@ skip_if_no_ff1 <- function() {
     skip("fastf1 not available for testing")
 }
 
-test_that("load race laps works", {
+test_that("load session laps works", {
   skip_if_no_ff1()
 
   # Set testing specific parameters - this disposes after the test finishes
@@ -19,14 +19,22 @@ test_that("load race laps works", {
   withr::local_options(f1dataR.cache = file.path(getwd(), "tst_session"))
 
 
-  laps <- load_race_laps(season = 2022, race = 'bahrain')
-  laps2 <- load_race_laps(season = 2022, race = 'bahrain', add_weather = T)
-  lapsq <- load_race_laps(season = 2022, race = 'bahrain', session = 'Q')
+  laps <- load_session_laps(season = 2022, race = 'bahrain')
+  laps2 <- load_session_laps(season = 2022, race = 'bahrain', add_weather = T)
+  lapsq <- load_session_laps(season = 2022, race = 'bahrain', session = 'Q')
+  lapsqw <- load_session_laps(season = 2022, race = 'bahrain', session = 'Q', add_weather = TRUE)
 
   expect_equal(class(laps), 'data.frame')
   expect_true(ncol(laps) %in% c(28,32))
   expect_true(ncol(laps2) %in% c(35, 39))
+  expect_equal(laps$Time[1], 3855.422)
+  expect_equal(laps$Time, laps2$Time)
   expect_equal(nrow(laps), nrow(laps2))
   expect_true(all(lapsq$SessionType %in% c('Q1', 'Q2', 'Q3')))
-  expect_true(all(c('Q1', 'Q2', 'Q3') %in% unique(laps1$SessionType)))
+  expect_true(all(c('Q1', 'Q2', 'Q3') %in% unique(lapsq$SessionType)))
+  expect_equal(lapsq$Time[1], 1404.977)
+  expect_equal(min(lapsq$LapTime, na.rm = TRUE), 90.558)
+  expect_equal(nrow(lapsq), nrow(lapsqw))
+  expect_equal(min(lapsq$LapTime, na.rm = TRUE), min(lapsqw$LapTime, na.rm = TRUE))
+
 })
