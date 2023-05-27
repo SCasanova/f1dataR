f4a390323337a4a76fe3abe5bc96474385b04fc6
Philip Bulsink <bulsinkp@gmail.com>
2023-05-26 20:44:18 -0400

    Test to match the fix

--- tests/testthat/test-load_quali.R
+++ tests/testthat/test-load_quali.R
@@ -12,5 +12,7 @@ test_that("Quali Load works", {
   expect_equal(nrow(quali_2004), 20)
   expect_equal(quali_2004$driverId[2], 'barrichello')
 
+  expect_equal(nrow(load_quali(2015, 16)), 20)
+
   expect_error(load_quali(3050, 2), "Year must be between 2003 and *")
 })
