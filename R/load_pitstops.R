f1f8d38ee8d6bae3a470f581aaf6327ed6b5d5b9
Santiago Casanova <santiago.casanova@yahoo.com>
2023-06-09 21:03:45 +0200

    Deprecate "race" as an argument in favour of "round" with proper documentation

--- R/load_pitstops.R
+++ R/load_pitstops.R
@@ -7,20 +7,27 @@
 #' @param season number from 2011 to current season (defaults to current season).
 #' @param race number from 1 to 23 (depending on season selected) and defaults
 #' to most recent.
+#' @param round number from 1 to 23 (depending on season selected) and defaults
+#' to most recent.
 #' @importFrom magrittr "%>%"
 #' @return A dataframe with columns driverId, lap, stop (number), time (of day),
 #' and stop duration
 
-.load_pitstops <- function(season = 'current', race  ='last'){
+.load_pitstops <- function(season = 'current', round  ='last', race = lifecycle::deprecated()){
+  if (lifecycle::is_present(race)) {
+    lifecycle::deprecate_warn("0.4.1", "load_pitstops(race)", "load_pitstops(round)")
+    round <- race
+  }
   if(season != 'current' & (season < 2011 | season > get_current_season())){
     stop(glue::glue('Year must be between 1950 and {current} (or use "current")',
                     current=get_current_season()))
   }
 
-  url <- glue::glue('{season}/{race}/pitstops.json?limit=80',
-                    season = season, race = race)
+  url <- glue::glue('{season}/{round}/pitstops.json?limit=80',
+                    season = season, round = round)
   data <- get_ergast_content(url)
-  data$MRData$RaceTable$Races$PitStops[[1]] %>% tibble::as_tibble()
+  data$MRData$RaceTable$Races$PitStops[[1]] %>%
+    tibble::as_tibble()
 }
 
 #' Load Pitstop Data
@@ -30,6 +37,8 @@
 #'
 #' @param season number from 2012 to current season (defaults to current season).
 #' @param race number from 1 to 23 (depending on season selected) and defaults
+#' to most recent.
+#' @param round number from 1 to 23 (depending on season selected) and defaults
 #' to most recent.
 #' @return A dataframe with columns
 #' @export
