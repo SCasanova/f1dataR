be01e7989dfffd8b66ff229496b438f13c62f65d
Philip Bulsink <pbulsink@users.noreply.github.com>
2023-06-23 10:05:53 -0400

    Update load_pitstops.R

--- R/load_pitstops.R
+++ R/load_pitstops.R
@@ -1,8 +1,8 @@
-#' Load Pitstop Data (not cached)
+#' Load Pitstop Data
 #'
 #' Loads pit stop info (number, lap, time elapsed) for a given race
-#' in a season. Pit stop data is available from 2012 onward.
-#' This function does not export, only the cached version.
+#' in a season. Pit stop data is available from 2012 onward. 
+#' Call `.load_pitstops()` for an uncached version.
 #'
 #' @param season number from 2011 to current season (defaults to current season).
 #' @param race number from 1 to 23 (depending on season selected) and defaults
@@ -13,8 +13,7 @@
 #' @keywords internal
 #' @return A tibble with columns driver_id, lap, stop (number), time (of day),
 #' and stop duration
-
-.load_pitstops <- function(season = 'current', round  ='last', race = lifecycle::deprecated()){
+.load_pitstops <- function(season = get_current_season(), round  ='last', race = lifecycle::deprecated()){
   if (lifecycle::is_present(race)) {
     lifecycle::deprecate_warn("1.0.0", "load_pitstops(race)", "load_pitstops(round)")
     round <- race
@@ -33,18 +32,10 @@
     janitor::clean_names()
 }
 
-#' Load Pitstop Data
-#'
-#' Loads pit stop info (number, lap, time elapsed) for a given race
-#' in a season. Pit stop data is available from 2012 onward.
-#'
-#' @param season number from 2012 to current season (defaults to current season).
-#' @param race number from 1 to 23 (depending on season selected) and defaults
-#' to most recent.
-#' @param round number from 1 to 23 (depending on season selected) and defaults
-#' to most recent.
-#' @return A tibble with columns driver_id, lap, stop (number), time (of day),
-#' and stop duration
+#' @import .load_pitstops title description params return
 #' @export
-
+#' @examples
+#' # Load pitstops from the first race of 2023:
+#' load_pistops(season = 2023, round = 1)
+#' 
 load_pitstops <- memoise::memoise(.load_pitstops)
