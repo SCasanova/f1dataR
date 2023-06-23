61265cfaad1cb07de5be13122c8d1eeb451c402b
Philip Bulsink <pbulsink@users.noreply.github.com>
2023-06-23 15:17:03 -0400

    Update load_results.R

--- R/load_results.R
+++ R/load_results.R
@@ -1,16 +1,15 @@
-#' Load Results (not cached)
+#' Load Results
 #'
-#' Loads final race results for a given year and round.
+#' Loads final race results for a given year and round. Use `.load_results()` for an uncached version
 #'
-#' @param season number from 1950 to current season  (defaults to current season).
+#' @param season number from 1950 to current season (or the word 'current') (defaults to current season).
 #' @param round number from 1 to 23 (depending on season), and defaults
-#' to most recent.
+#' to most recent (via the word 'last').
 #' @importFrom magrittr "%>%"
 #' @importFrom rlang .data
 #' @keywords internal
 #' @return A tibble with one row per driver
-
-.load_results <- function(season = 'current', round = 'last'){
+.load_results <- function(season = get_current_season(), round = 'last'){
   if(season != 'current' & (season < 1950 | season > get_current_season())){
     cli::cli_abort('{.var season} must be between 1950 and {get_current_season()} (or use "current")')
     # stop(glue::glue('Year must be between 1950 and {current} (or use "current")',
@@ -55,18 +54,18 @@
       tibble::as_tibble() %>%
       janitor::clean_names()
   }
-
 }
 
-#' Load Results
-#'
-#' Loads final race results for a given year and round.
-#'
-#' @param season number from 1950 to current season (defaults to current season).
-#' @param round number from 1 to 23 (depending on season), and defaults
-#' to most recent.
-#' @return A tibble with one row per driver
+#' @import .load_results title description params return
 #' @export
-
+#' @examples
+#' # Load results from the third race of 2023
+#' load_results(2023, 3)
+#' 
+#' # Load results from the first race of 1995
+#' load_results(1995, 1)
+#' 
+#' # Load results from the last race of 1963
+#' load_results(1963, 'last')
+#' 
 load_results <- memoise::memoise(.load_results)
-
