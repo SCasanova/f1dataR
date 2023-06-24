8a427981cbbc052ee74a5fa0daf492e70126facf
Philip Bulsink <pbulsink@users.noreply.github.com>
2023-06-24 11:26:36 -0400

    fixed other example & doc issues.
    All examples run locally now...

--- R/load_standings.R
+++ R/load_standings.R
@@ -3,7 +3,7 @@
 #' Loads standings at the end of a given season and round for drivers' or
 #' constructors' championships. Use `.load_standings()` for an uncached version of this function.
 #'
-#' @param season number from 1950 to current season (defaults to current season).
+#' @param season number from 2003 to current season (defaults to current season).
 #' @param round number from 1 to 23 (depending on season), and defaults
 #' to most recent. Also accepts `'last'`.
 #' @param type select `'driver'` or `'constructor'` championship data. Defaults to
@@ -14,7 +14,7 @@
 #' points, wins (and constructorsId in the case of drivers championship).
 .load_standings <- function(season = get_current_season(), round = 'last', type = 'driver'){
   if(season != 'current' & (season < 2003 | season > get_current_season())){
-    cli::cli_abort('{.var season} must be between 1950 and {get_current_season()} (or use "current")')
+    cli::cli_abort('{.var season} must be between 2003 and {get_current_season()} (or use "current")')
   }
 
   if(!(type %in% c('driver', 'constructor'))){
@@ -52,6 +52,6 @@
 #' # Get the driver standings at the end of 2021
 #' load_standings(2021, 'last', 'driver')
 #'
-#' # Get constructor standings at the end of 1997
-#' load_standings(1997, 'last', 'constructors')
+#' # Get constructor standings at part way through 2004
+#' load_standings(2004, 5, 'constructor')
 load_standings <- memoise::memoise(.load_standings)
