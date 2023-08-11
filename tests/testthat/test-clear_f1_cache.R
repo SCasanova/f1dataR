e425d7ea2e1aa5b42d30d1641f5c7b1a0dee68ff
Philip Bulsink <bulsinkp@gmail.com>
2023-08-10 22:00:19 -0400

    Remove httr2 cache files in `clear_cache()`
    Note: remainder of tests should be given withr::local_options too

--- tests/testthat/test-clear_f1_cache.R
+++ tests/testthat/test-clear_f1_cache.R
@@ -1,9 +1,21 @@
 test_that("Cache Clearing works for memoised functions", {
   # Note: cache clearing for fastf1 is not our responsibility, it's performed
   # by a call to fastf1 itself.
+
+  # Set testing specific parameters - this disposes after the test finishes
+  if (dir.exists(file.path(getwd(), "tst_clear_cache"))) {
+    unlink(file.path(getwd(), "tst_clear_cache"), recursive = TRUE, force = TRUE)
+  }
+  withr::local_file(file.path(getwd(), "tst_clear_cache"))
+  dir.create(file.path(getwd(), "tst_clear_cache"), recursive = TRUE)
+  withr::local_options(f1dataR.cache = file.path(getwd(), "tst_clear_cache"))
+
   expect_false(memoise::has_cache(load_schedule)())
   tmp <- load_schedule()
   expect_true(memoise::has_cache(load_schedule)())
+  expect_true(dir.exists(file.path(get_options('f1dataR.cache'), 'f1dataR_http_cache')))
   clear_f1_cache()
   expect_false(memoise::has_cache(load_schedule)())
+  expect_false(dir.exists(file.path(get_options('f1dataR.cache'), 'f1dataR_http_cache')))
+
 })
