d0b9ab449c37c638b327a9e1eb28f110866d899a
Santiago Casanova <santiago.casanova@yahoo.com>
2023-08-12 11:34:23 -0600

    Set `tempdir()` as default for cache and add startup message

--- R/zzz.R
+++ R/zzz.R
@@ -3,13 +3,18 @@
 }
 
 .onAttach <- function(libname, pkgname) {
+  # Give instructions on how to set up cache directory
+  packageStartupMessage(
+    'f1dataR cache is set up on a temporary directory. It is recommended to use `options(f1dataR.cache = path/to/dir/)` to set a permanent cache'
+    )
+
   # Load preexisting session/user options
   op <- options()
 
-  # Make our own lost of options
-  op_package <- list(f1dataR.cache = getwd())
+  # Make our own list of options
+  op_package <- list(f1dataR.cache = tempdir())
 
-  # IFF our options aren't already set, we'll include those in the new options to write
+  # IF our options aren't already set, we'll include those in the new options to write
   toset <- !(names(op_package) %in% names(op))
   if (any(toset)) {
     options(op_package[toset])
