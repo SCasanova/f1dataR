4600b4bbf1dcbf81e7bde15e034400a2f10ec2c1
Philip Bulsink <bulsinkp@gmail.com>
2023-08-12 20:32:56 -0400

    Styled & Linted

--- R/zzz.R
+++ R/zzz.R
@@ -4,75 +4,102 @@
 
   memoise_option <- getOption("f1dataR.cache", default = "memory")
 
-  if(!memoise_option %in% c('memory', 'filesystem', 'off')){
-    if(!dir.exists(memoise_option)) {
+  if (!memoise_option %in% c("memory", "filesystem", "off")) {
+    if (!dir.exists(memoise_option)) {
       cli::cli_alert_warning("Option 'f1dataR.cache' was set to {cache}.
                              It should be one of c('memory', 'filesystem', 'off') or
                              a valid path. Reverting to 'memory'.")
-      memoise_option <- 'memory'
+      memoise_option <- "memory"
     }
   }
 
-  if(!memoise_option %in% c('memory', 'off')){
+  if (!memoise_option %in% c("memory", "off")) {
     # memoise_option could be a valid dir or the string 'filepath'. If the latter, create a user_cache_dir
-    if(memoise_option == 'filesystem'){
+    if (memoise_option == "filesystem") {
       cache_dir <- rappdirs::user_cache_dir(appname = "f1dataR")
       dir.create(cache_dir, recursive = TRUE, showWarnings = FALSE)
 
-      #set the cachedir to our new location for fastf1 caching too
-      options('f1dataR.cache') <- cache_dir
+      # set the cachedir to our new location for fastf1 caching too
+      options("f1dataR.cache") <- cache_dir
     }
     cache <- cachem::cache_disk(dir = memoise_option)
-  } else if (memoise_option == 'memory'){
+  } else if (memoise_option == "memory") {
     cache <- cachem::cache_mem()
   }
 
-  if(memoise_option != "off"){
+  if (memoise_option != "off") {
     # 86400 s is 24h
-    assign(x = "load_circuits",
-           value = memoise::memoise(load_circuits, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_constructors",
-           value = memoise::memoise(load_constructors, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_driver_telemetry",
-           value = memoise::memoise(load_driver_telemetry, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_drivers",
-           value = memoise::memoise(load_drivers, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_laps",
-           value = memoise::memoise(load_laps, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_pitstops",
-           value = memoise::memoise(load_pitstops, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_quali",
-           value = memoise::memoise(load_quali, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_race_session",
-           value = memoise::memoise(load_race_session, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_results",
-           value = memoise::memoise(load_results, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_schedule",
-           value = memoise::memoise(load_schedule, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_session_laps",
-           value = memoise::memoise(load_session_laps, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_sprint",
-           value = memoise::memoise(load_sprint, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "load_standings",
-           value = memoise::memoise(load_standings, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
-    assign(x = "plot_fastest",
-           value = memoise::memoise(plot_fastest, ~ memoise::timeout(86400), cache = cache),
-           envir = rlang::ns_env("f1dataR"))
+    assign(
+      x = "load_circuits",
+      value = memoise::memoise(load_circuits, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_constructors",
+      value = memoise::memoise(load_constructors, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_driver_telemetry",
+      value = memoise::memoise(load_driver_telemetry, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_drivers",
+      value = memoise::memoise(load_drivers, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_laps",
+      value = memoise::memoise(load_laps, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_pitstops",
+      value = memoise::memoise(load_pitstops, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_quali",
+      value = memoise::memoise(load_quali, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_race_session",
+      value = memoise::memoise(load_race_session, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_results",
+      value = memoise::memoise(load_results, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_schedule",
+      value = memoise::memoise(load_schedule, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_session_laps",
+      value = memoise::memoise(load_session_laps, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_sprint",
+      value = memoise::memoise(load_sprint, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "load_standings",
+      value = memoise::memoise(load_standings, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
+    assign(
+      x = "plot_fastest",
+      value = memoise::memoise(plot_fastest, ~ memoise::timeout(86400), cache = cache),
+      envir = rlang::ns_env("f1dataR")
+    )
   }
-
 }
 
 .onAttach <- function(libname, pkgname) {
