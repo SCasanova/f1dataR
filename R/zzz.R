9b37d21df753e015c52497390b23e47deebf3ab7
Santiago Casanova <santiago.casanova@yahoo.com>
2023-08-13 19:28:39 -0600

    Typos and message in zzz

--- R/zzz.R
+++ R/zzz.R
@@ -3,17 +3,22 @@
 }
 
 .onAttach <- function(libname, pkgname) {
+  # Give instructions on how to set up cache directory
+  packageStartupMessage(
+    'f1dataR cache is set up on a temporary directory. It is recommended to use `options(f1dataR.cache = path/to/dir/)` to set a permanent cache'
+  )
+  
   # Load preexisting session/user options
   op <- options()
-
-  # Make our own lost of options
+  
+  # Make our own list of options
   op_package <- list(f1dataR.cache = getwd())
-
-  # IFF our options aren't already set, we'll include those in the new options to write
+  
+  # If our options aren't already set, we'll include those in the new options to write
   toset <- !(names(op_package) %in% names(op))
   if (any(toset)) {
     options(op_package[toset])
   }
-
+  
   invisible()
 }
