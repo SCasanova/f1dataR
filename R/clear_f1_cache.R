ec2cf3a4fcd99887fd09b13ceaaf038ffa6fa99c
Santiago Casanova <santiago.casanova@yahoo.com>
2023-08-13 20:53:36 -0600

    backslah escape

--- R/clear_f1_cache.R
+++ R/clear_f1_cache.R
@@ -19,11 +19,11 @@ clear_f1_cache <- function() {
       # check OS for path
         if (get_fastf1_version() >= 3) {
           reticulate::py_run_string(glue::glue("fastf1.Cache.clear_cache('{cache_dir}')",
-                                               cache_dir = stringr::str_replace(getOption("f1dataR.cache"), '\\', '/')
+                                               cache_dir = stringr::str_replace(getOption("f1dataR.cache"), '\\\\', '/')
           ))
         } else {
           reticulate::py_run_string(glue::glue("fastf1.api.Cache.clear_cache('{cache_dir}')",
-                                               cache_dir = stringr::str_replace(getOption("f1dataR.cache"), '\\', '/')
+                                               cache_dir = stringr::str_replace(getOption("f1dataR.cache"), '\\\\', '/')
           ))
         }
       } else{
