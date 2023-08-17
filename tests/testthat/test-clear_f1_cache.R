409c49d48bda7b7efb5ee2f967cab0cc778b08f4
Philip Bulsink <bulsinkp@gmail.com>
2023-08-17 06:37:08 -0700

    Add change_cache function (and clear_cache alias)

--- tests/testthat/test-clear_f1_cache.R
+++ tests/testthat/test-clear_f1_cache.R
@@ -16,3 +16,47 @@ test_that("Cache Clearing works for memoised functions to file", {
   clear_f1_cache()
   expect_false(memoise::has_cache(load_schedule)())
 })
+
+
+test_that("load_ciruits (off cache) works", {
+  # Set testing specific parameters - this disposes after the test finishes
+  change_cache("off", persist = FALSE)
+
+  ciruits_2021 <- load_circuits(2021)
+
+  expect_equal(nrow(ciruits_2021), 21)
+  expect_equal(ciruits_2021$circuit_id[3], "baku")
+  expect_equal(ciruits_2021$locality[1], "Austin")
+})
+
+
+test_that("load_ciruits (memory cache) works", {
+  # Set testing specific parameters - this disposes after the test finishes
+  change_cache("memory")
+
+  ciruits_2021 <- load_circuits(2021)
+
+  expect_equal(nrow(ciruits_2021), 21)
+  expect_equal(ciruits_2021$circuit_id[3], "baku")
+  expect_equal(ciruits_2021$locality[1], "Austin")
+
+  expect_equal(getOption("f1dataR.cache"), "memory")
+})
+
+
+test_that("load_ciruits (bad path cache) works", {
+  # Set testing specific parameters - this disposes after the test finishes
+  expect_error(change_cache("fakedirectory"), "Attempt to set cache to fakedirectory failed*")
+})
+
+
+test_that("load_ciruits (filesystem cache) works", {
+  # Set testing specific parameters - this disposes after the test finishes
+  change_cache(cache = 'filesystem')
+
+  ciruits_2021 <- load_circuits(2021)
+
+  expect_equal(nrow(ciruits_2021), 21)
+  expect_equal(ciruits_2021$circuit_id[3], "baku")
+  expect_equal(ciruits_2021$locality[1], "Austin")
+})
