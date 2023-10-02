5554b6652bc53fcb8081cffd5bc08c8d9748c16b
pbulsink <pbulsink@users.noreply.github.com>
2023-10-02 14:56:18 +0000

    Style & Document code

--- R/utils.R
+++ R/utils.R
@@ -167,10 +167,10 @@ setup_fastf1 <- function(..., envname = "f1dataR_env", new_env = identical(envna
 
 
 #' @noRd
-dummy <- function(){
-  #this function creates and cleans a dummy table to clear r cmd check notes for
-  #janitor, tibble and tidyr
-  dummy<-tibble::tibble("Dummy" = c(1:5, NA)) %>%
+dummy <- function() {
+  # this function creates and cleans a dummy table to clear r cmd check notes for
+  # janitor, tibble and tidyr
+  dummy <- tibble::tibble("Dummy" = c(1:5, NA)) %>%
     janitor::clean_names() %>%
     tidyr::drop_na()
   invisible(NULL)
