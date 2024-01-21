da8362e3057e4da09e2953213be40f7c61c947c8
Santiago Casanova <santiago.casanova@yahoo.com>
2024-01-20 20:21:33 -0600

    test

--- R/utils.R
+++ R/utils.R
@@ -17,6 +17,7 @@ get_ergast_content <- function(url) {
   # Automatically retries request up to 5 times. Backoff provided in httr2 documentation
   # Automatically retries at http if https fails after retries.
 
+  
   ergast_raw <- httr2::request("https://ergast.com/api/f1") %>%
     httr2::req_url_path_append(url) %>%
     httr2::req_retry(max_tries = 5) %>%
