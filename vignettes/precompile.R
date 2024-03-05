0041c8299733c23c014f66131530eb3f451c463d
Philip Bulsink <pbulsink@users.noreply.github.com>
2024-03-04 21:05:41 -0500

    Document circuit details with Vignette (#223)
    * make circuit_details return a named list
    
    * Add circuit_details to readme
    
    * Style & Document code
    
    * Built Readme
    
    * Update PlotFastest to just move margins, not shift plot data
    
    This way, if there are labels/annotations on the plot, they don't need to be moved too.
    
    * Update NEWS
    
    * load_circuit_details() named items
    * correct_track_ratio() changes
    
    * Further improve plotting and circuit_details
    
    * Create plotting-turn-info.Rmd.orig
    
    * Style & Document code
    
    * rebuilt all vignettes & readme
    
    * Added figure captions and rebuilt vignettes.
    
    Included other minor text changes.
    
    * Update NEWS.md
    
    * added small buffer to plots to prevent label clipping
    
    see vignettes/plotting-turn-info-relabelled_gear_plot-1.png change
    
    * Add circuit_details to pkgdown spec

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -8,13 +8,15 @@
 
 knitr::knit("vignettes/ergast-data-analysis.Rmd.orig", "vignettes/ergast-data-analysis.Rmd")
 knitr::knit("vignettes/introduction.Rmd.orig", "vignettes/introduction.Rmd")
+knitr::knit("vignettes/plotting-turn-info.Rmd.orig", "vignettes/plotting-turn-info.Rmd")
 cat("Successfully compiled vignettes. Now moving figures to ./vignettes")
 
-vig_images<-list.files(pattern = "(ergast-data-analysis|introduction)-[(a-z)(A-Z)(0-9)_]*-1.png")
+vig_images<-list.files(pattern = "(ergast-data-analysis|introduction|plotting-turn-info)-[(a-z)(A-Z)(0-9)_]*-1.png")
 file.copy(paste0("./", vig_images),
           paste0("./vignettes/", vig_images), overwrite = T)
 unlink(vig_images)
-# If you have the optimpng tool installed on your system run this to reduce png size
+# If you have the optipng tool installed on your system run this to reduce png size
+# xfun::optipng("./vignettes/")
 
 # Optional
 devtools::build_vignettes()
