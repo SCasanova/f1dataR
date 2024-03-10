2aa71b6e2189ab081b3d6a6ca9c92d1a91b92312
Philip Bulsink <bulsinkp@gmail.com>
2024-03-10 19:44:28 -0400

    Update constructors to 2024
    #235

--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -1,3 +1,4 @@
-constructor_data <- readr::read_csv("data-raw/constructor_data.csv")
+constructor_data <- readr::read_csv("data-raw/constructor_data.csv") %>%
+  unique()
 
 usethis::use_data(constructor_data, overwrite = TRUE)
